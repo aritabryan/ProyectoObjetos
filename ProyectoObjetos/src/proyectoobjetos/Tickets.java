11ae905b238c716edaeb1d9b010ee4f7d946d438
gsussman05 <147879065+gsussman05@users.noreply.github.com>
2024-03-13 21:51:28 -0600

    Add files via upload

--- ProyectoObjetos/src/proyectoobjetos/Tickets.java
+++ ProyectoObjetos/src/proyectoobjetos/Tickets.java
@@ -1,23 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package proyectoobjetos;
 
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Gabriel
+ */
 public class Tickets {
 
     private double precio;
     private double vuelto;
-    private String asiento;
+    private String clase;
     private int cantidadTickets;
     private String destino;
+    private String horario;
 
-    public Tickets(double precio, double vuelto, String asientos, int cantidadTickets, String destino) {
-        this.precio = 9.99;
-        this.vuelto = vuelto;
-        this.asiento = asientos;
+    public Tickets(double precio, String clase, int cantidadTickets, String destino, String horario) {
+        this.precio = precio;
+        this.clase = clase;
         this.cantidadTickets = cantidadTickets;
         this.destino = destino;
+        this.horario = horario;
     }
 
     public double getPrecio() {
         return precio;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
     public double getVuelto() {
@@ -28,36 +47,37 @@ public class Tickets {
         return destino;
     }
 
-    public String getAsientos() {
-        return asiento;
+    public String clase() {
+        return clase;
     }
 
     public int getCantidadTickets() {
         return cantidadTickets;
     }
 
-    public void vuelto(int recibido, int cantidadTickets) {
-        vuelto = recibido - precio*cantidadTickets;
+    public void vuelto(double recibido, int cantidadTickets) {
+        if (recibido < (precio * cantidadTickets)) {
+            JOptionPane.showMessageDialog(null, "No ha pagado el precio completo del ticket");
+            return;
+        }
+
+        vuelto = recibido - (precio * cantidadTickets);
         if (vuelto < 0) {
-            System.out.println("No ha pagado el precio completo del ticket");
+            JOptionPane.showMessageDialog(null, "No ha pagado el precio completo del ticket");
         } else if (vuelto == 0) {
-            System.out.println("Gracias, Ha pagado por completo el ticket");
+            JOptionPane.showMessageDialog(null, "Gracias, Ha pagado por completo el ticket");
         } else {
-            System.out.println("Gracias, Su vuelto es de: " + vuelto);
+            JOptionPane.showMessageDialog(null, "Gracias, Su vuelto es de: " + vuelto);
         }
     }
 
-//    public void AsientoDisponible(String asientoIngresado, String destinoIngresado) {
-//        if (asientoIngresado.equals(asiento) && destinoIngresado.equals(destino)) {
-//            System.out.println("Asiento ocupado!!");
-//        }else{
-//            System.out.println("Ha reservado el asiento "+asientoIngresado+" con destino a "+destinoIngresado);
-//        }
-//    }
-
-    @Override
-    public String toString() {
-        return "Tickets{" + ", cantidadTickets=" + cantidadTickets + "precio=" + precio + ", vuelto=" + vuelto + ", asientos=" + asiento + ", destino=" + destino + '}';
+    public double getTotalPrice() {
+        return precio * cantidadTickets;
     }
 
+    @Override
+public String toString() {
+        return "Tickets [cantidadTickets=" + cantidadTickets + ", clase=" + clase + ", destino=" + destino + ", horario="
+                + horario + ", precio=" + precio + "]";
+    }
 }
